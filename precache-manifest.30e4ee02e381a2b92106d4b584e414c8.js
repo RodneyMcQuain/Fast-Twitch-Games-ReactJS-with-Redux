f11192d802152d2e4d2176d210bb344a4399bcba
Rodney McQuain <rodneymcquain95@gmail.com>
2018-11-25 23:50:47 -0500

    Updates

---
+++ precache-manifest.30e4ee02e381a2b92106d4b584e414c8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4c653a2a4d9528cf6ac7",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/css/main.902f10ab.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2f7d6ec7557e72f6a33e",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/js/1.2f7d6ec7.chunk.js"
+  },
+  {
+    "revision": "8598fd97ec8f2d8f5e1e",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/js/runtime~main.8598fd97.js"
+  },
+  {
+    "revision": "4c653a2a4d9528cf6ac7",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/js/main.4c653a2a.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2f7d6ec7557e72f6a33e",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/static/css/1.a97c5214.chunk.css"
+  },
+  {
+    "revision": "1b2ef2f0ed21af4233fb52e28532b6a6",
+    "url": "/Fast-Twitch-Games-ReactJS-with-Redux/index.html"
+  }
+];
